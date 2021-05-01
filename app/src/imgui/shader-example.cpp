d9fe116488866899b2c83895c8a2891703294567
yushiang <tsengyushiang@gmail.com>
2021-05-01 22:58:39 +0800

    add : opengl shader example

--- app/src/imgui/shader-example.cpp
+++ app/src/imgui/shader-example.cpp
@@ -1,4 +1,4 @@
-﻿// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
 // (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
 // If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
@@ -9,45 +9,51 @@
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
 
-// About Desktop OpenGL function loaders:
-//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
-//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
-//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include <GL/gl3w.h>            // Initialize with gl3wInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-#include <GL/glew.h>            // Initialize with glewInit()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-#include <glad/glad.h>          // Initialize with gladLoadGL()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
-#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
-#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
-#include <glbinding/gl/gl.h>
-using namespace gl;
-#else
-#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
-#endif
-
-// Include glfw3.h after our OpenGL definitions
-#include <GLFW/glfw3.h>
-
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
+#include <GLFW/glfw3.h>         // Include glfw3.h after our OpenGL definitions
 
 #include <iostream>
 #include <string>
 #include <vector>
+
+namespace CameraParams {
+
+    float Sensitivity = 1e-2;
+    float r = 3;
+    float v = 0;
+    float h = 0;
+
+    glm::vec3 Spherical2CartesianCoordinates() {
+        glm::vec3 pos(r * sin(h) * cos(v), r * sin(h) * sin(v), r * cos(h));
+        return pos;
+    }
+
+    glm::mat4 getMVP(float display_w,float display_h) {
+        glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float)display_w / (float)display_h, 0.1f, 100.0f);
+        glm::mat4 View = glm::lookAt(
+            Spherical2CartesianCoordinates(), // Camera is at (4,3,3), in World Space
+            glm::vec3(0, 0, 0), // and looks at the origin
+            glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+        );
+        glm::mat4 Model = glm::mat4(1.0f);
+        return Projection * View * Model; // Remember, matrix multiplication is the other way around
+    }
+
+    void rotatecam(float deltax, float deltay) {
+        if (deltax > 0)
+            v += Sensitivity;
+        else
+            v -= Sensitivity;
+
+        if (deltay > 0)
+            h += Sensitivity;
+        else
+            h -= Sensitivity;
+    }
+}
 
 static void glfw_error_callback(int error, const char* description)
 {
@@ -137,10 +143,11 @@ int glfw_shader_example()
         "#version 330\n"
         "layout(location = 0) in vec4 vposition;\n"
         "layout(location = 1) in vec4 vcolor;\n"
+        "uniform mat4 MVP;"
         "out vec4 fcolor;\n"
         "void main() {\n"
         "   fcolor = vcolor;\n"
-        "   gl_Position = vposition;\n"
+        "   gl_Position =  MVP * vposition;\n"
         "}\n";
 
     std::string fragment_source =
@@ -239,6 +246,15 @@ int glfw_shader_example()
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
 
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+       
+        glm::mat4 mvp = CameraParams::getMVP(display_w, display_h);
+
+        GLuint MatrixID = glGetUniformLocation(shader_program, "MVP");
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
         // use the shader program
         glUseProgram(shader_program);
         // bind the vao
@@ -251,6 +267,11 @@ int glfw_shader_example()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        if (ImGui::IsMouseDragging(0)) {
+            ImVec2 mousedelta = ImGui::GetMouseDragDelta();
+            //CameraParams::rotatecam(mousedelta.x, mousedelta.y);
+        }
+
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             static float f = 0.0f;
@@ -261,6 +282,13 @@ int glfw_shader_example()
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 
             ImGui::SliderFloat("float", &f, 0.0f, 0.5f);            // Edit 1 float using a slider from 0.0f to 0.5f
+
+            ImGui::Text("Camera parameters.");               // Display some text (you can use a format strings too)
+            ImGui::SliderFloat("distance", &CameraParams::r, 0.0f, 5.0f);            // Edit 1 float using a slider from 0.0f to 0.5f
+            ImGui::SliderFloat("PolarAngle", &CameraParams::v, -3.14f, 3.14f);            // Edit 1 float using a slider from 0.0f to 0.5f
+            ImGui::SliderFloat("AzimuthAngle", &CameraParams::h, -3.14f, 3.14f);            // Edit 1 float using a slider from 0.0f to 0.5f
+
+
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
             if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
